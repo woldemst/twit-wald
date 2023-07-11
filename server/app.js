529116336df7296b3ca5f0c4cf0e89986dbaf3bb
woldemst <adlerrein@gmail.com>
2023-07-11 10:21:38 +0200

    Sarted working on backed | nodemon body-parser express mongoode dependencies were installed | HttpError utility were added to the backend

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const jsonwebtoken = require("jsonwebtoken")
+const bodyParser = require("body-parser")
+const app = express();
+
+app.use((error, req, res, next) => {
+    if(res.headerSent){
+        return next(error)
+    }
+    res.status(error.code || 500)
+    res.json({message: error.message || 'An uknown error occured!'})
+})
+app.listen(8000)
+
+
+// mongoose
+//     .connect(
+//     "mongodb+srv://woldemst:WeinertW1480@twittcluster.av2nweb.mongodb.net/?retryWrites=true&w=majority",
+//     {
+//         useNewUrlParser: true,
+//         useUnifieldTopology: true,
+//         writeConcern: { w: "majority" },
+//     }
+//     )
+//     .then(()=>{
+//         app.listen(8000)
+//         console.log("port connected");
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     })
+
+
+
