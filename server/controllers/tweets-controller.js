921e49d9ee547985edbaee0b651841a210fc8e2b
woldemst <adlerrein@gmail.com>
2023-07-24 16:16:41 +0200

    Added tweet controllers

---
+++ server/controllers/tweets-controller.js
@@ -0,0 +1,11 @@
+const HttpError = require('../models/http-error')
+
+const getAllTweets = async (req, res, next) => {
+    try {
+        
+    } catch (err) {
+        const error = await 
+        
+    }
+}
+
