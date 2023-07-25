a0bdd3c968ee6892c9a2fd5b6a39637d962f76e6
woldemst <adlerrein@gmail.com>
2023-07-25 10:48:24 +0200

    Tweets from db will be shown on home page

--- client/src/layout/columns/Main.js
+++ client/src/layout/columns/Main.js
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import TweetList from "../../tweets/components/TweetList";
-import { TWEETS } from "../../user/pages/UserPage";
+
 import "./Main.scss";
 
 export default function Main() {
+  const [fetchedTweets, setFetchedTweets] = useState([])
+
+  useEffect(() => {
+
+  // function for fetching all of the tweets
+  const fetchTweets = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/tweets')
+      setFetchedTweets(response.data)
+    } catch (err) {
+      console.log('Error fetching tweets', err)
+      
+    }
+  }
+
+  fetchTweets()
+  }, [])
+
+  const loadedtweets = fetchedTweets.map((t) => t)
+  console.log(fetchedTweets);
   return (
     <>
       <div className="column main-content">
-        <TweetList items={TWEETS} />
+        <TweetList items={loadedtweets} />
       </div>
 
     </>
--- client/src/user/pages/UserPage.js
+++ client/src/user/pages/UserPage.js
@@ -8,66 +8,6 @@ import UsersList from "../components/UsersList";
 
 import TweetList from "../../tweets/components/TweetList";
 
-import image from "../../images/avatar.jpeg";
-
-export const TWEETS = [
-  {
-    id: "t1",
-    creatorId: "u1",
-    creator: {
-      image: image,
-      name: "Waldemar Weinert",
-      nickname: "@woldemst",
-    },
-    postedDate: "Jun 2",
-    description:
-      "It's just one more day. More work, more experience. More experience, more money",
-    imageUrl:
-      "https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=742&q=80",
-  },
-  {
-    id: "t2",
-    creatorId: "u2",
-    creator: {
-      image:
-        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=800",
-      name: "John Doe",
-      nickname: "@johndoe",
-    },
-    postedDate: "May 28",
-    description: "Enjoying a beautiful day at the beach",
-    imageUrl:
-      "https://pbs.twimg.com/media/FzKSc68WcAAJyEH?format=jpg&name=medium",
-  },
-  {
-    id: "t3",
-    creatorId: "u3",
-    creator: {
-      image:
-        "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=800",
-      name: "Jane Smith",
-      nickname: "@janesmith",
-    },
-    postedDate: "Jun 1",
-    description: "Exploring the stunning landscapes of the mountains",
-    imageUrl:
-      "https://pbs.twimg.com/media/FzKSc68WcAAJyEH?format=jpg&name=medium",
-  },
-  {
-    id: "t4",
-    creatorId: "u1",
-    creator: {
-      image: image,
-      name: "Alex Johnson",
-      nickname: "@alexjohnson",
-    },
-    postedDate: "May 30",
-    description: "Having a delicious meal at a fancy restaurant",
-    imageUrl:
-      "https://images.unsplash.com/photo-1661956601030-fdfb9c7e9e2f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=742&q=80",
-  },
-];
-
 const UserPage = (props) => {
   const { userId } = useParams();
 
@@ -90,7 +30,7 @@ const UserPage = (props) => {
         console.log('Error fetching users', err);
       }
     }
-
+    
     const fetchTweets = async () => {
       try {
         const response = await axios.get('http://localhost:8000/api/tweets')
